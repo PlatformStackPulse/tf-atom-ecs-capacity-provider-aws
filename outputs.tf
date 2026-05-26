828349dafbfe593bb213ec22bcbb5fff4d77eccb
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:14:41 +0100

    feat: implement tf-atom-ecs-capacity-provider-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "arn" {
+  description = "ARN of the capacity provider"
+  value       = try(aws_ecs_capacity_provider.this[0].arn, null)
+}
+
+output "name" {
+  description = "Name of the capacity provider"
+  value       = try(aws_ecs_capacity_provider.this[0].name, null)
 }
